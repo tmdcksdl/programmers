2d671d79414a75cb63dca0b85bc540813e579b09
tmdcks_99 <128008378+tmdcksdl@users.noreply.github.com>
2024-11-08 20:31:06 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+
+        long n = num; 
+        
+        while (n != 1) {
+            if (answer >= 500) {
+                return -1;
+            }
+
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = n * 3 + 1;
+            }
+
+            answer++;
+        }
+
+        return answer;
+    }
+}
