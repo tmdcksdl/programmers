cdaf37e2e616a55ff07b5044330908115eb69b27
tmdcks_99 <128008378+tmdcksdl@users.noreply.github.com>
2024-12-12 09:49:16 +0900

    [level 1] Title: 명예의 전당 (1), Time: 6.50 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        
+        ArrayList<Integer> list = new ArrayList<>(k);
+		
+        for (int i = 0; i < score.length; i++) {
+            list.add(score[i]);
+            Collections.sort(list);
+            if (list.size() > k) {
+                list.remove(0);
+            }
+            answer[i] = list.get(0);
+        }
+        return answer;
+    }
+}
