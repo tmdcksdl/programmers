2c6032a761aa049bc527ee90102bff2fda0cc3af
tmdcks_99 <128008378+tmdcksdl@users.noreply.github.com>
2025-04-15 20:06:48 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 9.74 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String t, String p) {
+        int answer = 0;
+
+        for (int i = 0; i < t.length() - p.length() + 1; i++) {
+            String subString = t.substring(i, i + p.length());
+            if (Long.parseLong(subString) <= Long.parseLong(p)) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
