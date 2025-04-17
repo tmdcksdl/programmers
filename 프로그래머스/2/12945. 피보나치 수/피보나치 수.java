76fb56255ab52acd2eca52916d9749198edf58b4
tmdcks_99 <128008378+tmdcksdl@users.noreply.github.com>
2025-04-17 14:32:32 +0900

    [level 2] Title: 피보나치 수, Time: 1.64 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        int num1 = 0;
+        int answer = 1;
+        int count = 1;
+        
+        while (count < n) {
+            int num = num1;
+            num1 = answer;
+            
+             answer = (num + num1) % 1234567;
+            
+            count++;
+        }
+        
+        return answer;
+    }
+}
