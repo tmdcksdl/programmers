1c8a96226705256e5e10d73229d5376125b9bf3d
tmdcks_99 <128008378+tmdcksdl@users.noreply.github.com>
2025-07-10 11:12:51 +0900

    [level 2] Title: 귤 고르기, Time: 52.51 ms, Memory: 90.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        Map<Integer, Integer> sizeCountMap = new HashMap<>();
+        
+        for (int t : tangerine) {
+            sizeCountMap.put(t, sizeCountMap.getOrDefault(t, 0) + 1);
+        }
+        
+        List<Integer> sizeCounts = new ArrayList<>(sizeCountMap.values());
+        sizeCounts.sort(Comparator.reverseOrder());
+        
+        for (int count : sizeCounts) {
+            k -= count;
+            answer++;
+            if (k <= 0) {
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
